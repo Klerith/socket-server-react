7aa23d5e99974dd5ec36da770a43dfac5b769fce
Fernando Herrera <fernando.herrera85@gmail.com>
2020-10-10 11:24:14 -0400

    Readme y limpieza

---
+++ README.md
@@ -0,0 +1,5 @@
+# Socket Server
+
+Este backend contiene todo lo necesario para configurar un servidor de express + socket.io.
+
+Cualquier conexión adicional de sockets, se puede hacer en el archivo ```models/sockets.js``` y cualquier middleware adicional de express, se puede realizar en el archivo ```models/server.js```
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+// Server Model: Contiene todo el servidor de express + socket.io configurado
 const Server = require('./models/server');
+
+// Paquete para leer y establecer las variables de entorno
 require('dotenv').config();
 
+
+// Inicializar la instancia del server
 const server = new Server();
 
-
+// Ejecutar el server
 server.execute();
 
 
